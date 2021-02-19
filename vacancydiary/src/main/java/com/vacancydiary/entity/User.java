4e9607d5d97c00f66306ca326866a1ad37b16358
Dima Kovlenko <qasper6@gmail.com>
2021-02-19 21:49:52 +0200

    mapped user and vacancy entities

---
+++ vacancydiary/src/main/java/com/vacancydiary/entity/User.java
@@ -0,0 +1,104 @@
+package com.vacancydiary.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "vacancies_users", joinColumns = @JoinColumn(name = "vacancy_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    List<Vacancy> vacancies;
+
+    public User() {
+    }
+
+    public User(Integer id, String login, String password, List<Vacancy> vacancies) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+        this.vacancies = vacancies;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Vacancy> getVacancies() {
+        return vacancies;
+    }
+
+    public void setVacancies(List<Vacancy> vacancies) {
+        this.vacancies = vacancies;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+
+        return Objects.equals(id, user.id) &&
+                Objects.equals(login, user.login) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, login, password);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", vacancies=" + vacancies +
+                '}';
+    }
+}
---
+++ vacancydiary/src/main/java/com/vacancydiary/entity/VacancyStatus.java
@@ -0,0 +1,12 @@
+package com.vacancydiary.entity;
+
+public enum VacancyStatus {
+    GIVE_IN,
+    GAVE_TEST,
+    WAITING_FEEDBACK,
+    SCREENING,
+    TECHNICAL_INTERVIEW,
+    OFFER,
+    REFUSAL,
+    NO_ANSWER
+}
